65ad3c82fa6e947fd917ea7a8a825a4ae3c9d00e
Tyler Safaric <tylersafaric99@gmail.com>
2020-06-22 15:31:38 -0400

    Stuff

---
+++ Assets/MoveScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] int DestX;
+    [SerializeField] int DestY;
+
+    Vector2 North; 
+
+    Grid grid;
+
+    Rigidbody2D dwarfBody;
+
+    void Start()
+    {
+        grid = GameObject.Find("Ground").GetComponent<Grid>();
+        dwarfBody = GetComponent<Rigidbody2D>();
+        North = new Vector2(0, grid.cellSize.y / 100);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dwarfBody.position += dwarfBody.position + North;
+    }
+}
